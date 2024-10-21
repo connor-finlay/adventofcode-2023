7893dff7f6d3b41f4ce5a8094798352e96905b81
connor-finlay <167970464+connor-finlay@users.noreply.github.com>
2024-10-22 08:59:32 +1100

    add regex to find number of green balls day2

--- day2/main.go
+++ day2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -11,8 +12,28 @@ func main() {
 		fmt.Println("Error compiling regex:", err)
 	}
 
-	id_matches := r.FindStringSubmatch("Game 1: 1 green, 6 red, 4 blue; 2 blue, 6 green, 7 red; 3 red, 4 blue, 6 green; 3 green; 3 blue, 2 green, 1 red")
+	/*
+		bag_red := 12
+		bag_green := 13
+		bag_blue := 14
+	*/
 
-	id := id_matches[1]
+	example_game := "Game 1: 1 green, 6 red, 4 blue; 2 blue, 6 green, 7 red; 3 red, 4 blue, 6 green; 3 green; 3 blue, 2 green, 1 red"
 
+	//id_matches := r.FindStringSubmatch(example_game)
+	// id := id_matches[1]
+
+	game_contents := strings.Split(example_game, ":")
+	games := strings.Split(game_contents[1], ";")
+
+	r, err = regexp.Compile(`(\d+) g`)
+	if err != nil {
+		fmt.Println("Error compiling regex:", err)
+	}
+
+	for i := range len(games) {
+		fmt.Println(games[i])
+		matches := r.FindStringSubmatch(games[i])
+		fmt.Println("There were ", matches[1], " green balls")
+	}
 }
